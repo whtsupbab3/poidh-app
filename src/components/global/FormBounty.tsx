47834faf43b15c8a3f089ee61cc130dfc0e5eb84
yukigesho <yukigesho@proton.me>
2024-11-13 19:30:39 -0800

    fix: issue #346

--- src/components/global/FormBounty.tsx
+++ src/components/global/FormBounty.tsx
@@ -1,10 +1,15 @@
-import { Box, CircularProgress, Switch } from '@mui/material';
+import {
+  Box,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Switch,
+} from '@mui/material';
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 
-import GameButton from '@/components/global/GameButton';
-import { InfoIcon } from '@/components/global/Icons';
-import ButtonCTA from '@/components/ui/ButtonCTA';
 import { useGetChain } from '@/hooks/useGetChain';
 import { useAccount, useSwitchChain, useWriteContract } from 'wagmi';
 import { useMutation } from '@tanstack/react-query';
@@ -14,8 +19,17 @@ import abi from '@/constant/abi/abi';
 import { cn } from '@/utils';
 import Loading from '@/components/global/Loading';
 import { trpcClient } from '@/trpc/client';
+import GameButton from '@/components/global/GameButton';
+import ButtonCTA from '@/components/ui/ButtonCTA';
+import { InfoIcon } from '@/components/global/Icons';
 
-export default function FormBounty() {
+export default function FormBounty({
+  open,
+  onClose,
+}: {
+  open: boolean;
+  onClose: () => void;
+}) {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [amount, setAmount] = useState('');
@@ -98,85 +112,107 @@ export default function FormBounty() {
   return (
     <>
       <Loading open={createBountyMutations.isPending} status={status} />
-      <div className='mt-10 flex text-left flex-col text-white rounded-[30px] border border-[#D1ECFF] p-5 w-full lg:min-w-[400px] justify-center backdrop-blur-sm bg-poidhBlue/60'>
-        <span>title</span>
-        <input
-          type='text'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className='border bg-transparent border-[#D1ECFF] py-2 px-2 rounded-md mb-4'
-        />
-        <span>description</span>
-        <textarea
-          rows={3}
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          className='border bg-transparent border-[#D1ECFF] py-2 px-2 rounded-md mb-4 max-h-28'
-        ></textarea>
+      <Dialog
+        open={open}
+        onClose={() => {
+          onClose();
+          setAmount('');
+        }}
+        maxWidth='xs'
+        fullWidth
+        PaperProps={{
+          className: 'bg-poidhBlue/50',
+          style: {
+            borderRadius: '10px',
+            color: 'white',
+            border: '1px solid #D1ECFF',
+          },
+        }}
+      >
+        <DialogTitle>create bounty</DialogTitle>
+        <DialogContent>
+          <Box display='flex' flexDirection='column' width='100%'>
+            <span>title</span>
+            <input
+              type='text'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className='border bg-transparent border-[#D1ECFF] py-2 px-2 rounded-md mb-4'
+            />
+            <span>description</span>
+            <textarea
+              rows={3}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className='border bg-transparent border-[#D1ECFF] py-2 px-2 rounded-md mb-4 max-h-28'
+            ></textarea>
 
-        <span>reward</span>
-        <input
-          type='number'
-          placeholder={`amount in ${chain.currency}`}
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          className='border bg-transparent border-[#D1ECFF] py-2 px-2 rounded-md mb-4'
-        />
-        <div className='flex text-balance gap-2 text-xs mb-2 items-center'>
-          <InfoIcon width={18} height={18} /> a 2.5% fee is deducted from
-          completed bounties
-        </div>
-
-        <div className='flex items-center justify-start gap-2'>
-          <span>{isSoloBounty ? 'Solo Bounty' : 'Open Bounty'}</span>
-          <Switch
-            checked={isSoloBounty}
-            onClick={() => setIsSoloBounty(!isSoloBounty)}
-            inputProps={{ 'aria-label': 'controlled' }}
-          />
-        </div>
-        <div className=' text-xs'>
-          <span className='flex gap-2 items-center max-w-md '>
-            <InfoIcon width={18} height={18} />
-            {isSoloBounty
-              ? 'you are the sole bounty contributor'
-              : 'users can add additional funds to your bounty'}
-          </span>
-        </div>
-
-        {createBountyMutations.isPending ? (
-          <Box className='flex justify-center items-center mt-5'>
-            <CircularProgress />
-          </Box>
-        ) : (
-          <button
-            className={cn(
-              'flex flex-row items-center justify-center',
-              account.isDisconnected && 'opacity-50 cursor-not-allowed'
-            )}
-            onClick={() => {
-              if (account.isConnected && name && description && amount) {
-                createBountyMutations.mutate();
-              } else {
-                toast.error(
-                  'Please fill in all fields and check wallet connection.'
-                );
-              }
-            }}
-            disabled={account.isDisconnected}
-          >
-            {createBountyMutations.isPending && (
-              <Box sx={{ display: 'flex' }}>
-                <CircularProgress />
-              </Box>
-            )}
-            <div className='button'>
-              <GameButton />
+            <span>reward</span>
+            <input
+              type='number'
+              placeholder={`amount in ${chain.currency}`}
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              className='border bg-transparent border-[#D1ECFF] py-2 px-2 rounded-md mb-4 placeholder:text-slate-400'
+            />
+            <div className='flex text-balance gap-2 text-xs mb-2 items-center'>
+              <InfoIcon width={18} height={18} /> a 2.5% fee is deducted from
+              completed bounties
             </div>
-            <ButtonCTA>create bounty</ButtonCTA>
-          </button>
-        )}
-      </div>
+            <div className='flex items-center justify-start gap-2'>
+              <span>{isSoloBounty ? 'Solo Bounty' : 'Open Bounty'}</span>
+              <Switch
+                checked={isSoloBounty}
+                onClick={() => setIsSoloBounty(!isSoloBounty)}
+                inputProps={{ 'aria-label': 'controlled' }}
+              />
+            </div>
+            <div className=' text-xs'>
+              <span className='flex gap-2 items-center max-w-md '>
+                <InfoIcon width={18} height={18} />
+                {isSoloBounty
+                  ? 'you are the sole bounty contributor'
+                  : 'users can add additional funds to your bounty'}
+              </span>
+            </div>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          {createBountyMutations.isPending ? (
+            <Box className='flex justify-center items-center mt-5'>
+              <CircularProgress />
+            </Box>
+          ) : (
+            <button
+              className={cn(
+                'flex flex-row items-center justify-center',
+                account.isDisconnected && 'opacity-50 cursor-not-allowed'
+              )}
+              onClick={() => {
+                if (account.isConnected && name && description && amount) {
+                  onClose();
+                  createBountyMutations.mutate();
+                } else {
+                  toast.error(
+                    'Please fill in all fields and check wallet connection.'
+                  );
+                }
+              }}
+              disabled={account.isDisconnected}
+            >
+              {createBountyMutations.isPending && (
+                <Box sx={{ display: 'flex' }}>
+                  <CircularProgress />
+                </Box>
+              )}
+              <div className='button'>
+                <GameButton />
+              </div>
+              <ButtonCTA>create bounty</ButtonCTA>
+            </button>
+          )}
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
